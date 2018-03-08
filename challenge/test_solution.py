a1e4a477ed819823903dcb3262eb0de6df4e0e04
Pascal van Kooten <kootenpv@gmail.com>
2018-03-09 00:59:36 +0100

    new attempt

--- challenge/test_solution.py
+++ challenge/test_solution.py
@@ -4,7 +4,7 @@ import glob
 
 list_of_files = glob.glob('solutions/**/')
 
-latest_sol_module = max(list_of_files, key=os.path.getctime)
+latest_sol_module = max(list_of_files, key=os.path.getmtime)
 
 sys.path = [os.path.dirname(latest_sol_module)] + ["."] + sys.path
 
